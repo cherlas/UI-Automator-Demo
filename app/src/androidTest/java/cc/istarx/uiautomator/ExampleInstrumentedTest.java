b4af98c2d30c48718d49c54bf830f2885ca083d1
cherlas <vm@li.cm>
2018-04-07 23:10:34 +0800

    add ui aiautomator test for demo

--- app/src/androidTest/java/cc/istarx/uiautomator/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/cc/istarx/uiautomator/ExampleInstrumentedTest.java
@@ -1,13 +1,18 @@
 package cc.istarx.uiautomator;
 
-import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.Until;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +21,30 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("cc.istarx.uiautomator", appContext.getPackageName());
+    private static final String PKG = "cc.istarx.uiautomator";
+    private static final String EMAIL = "istarx@163.com";
+    private static final String PASSWORD = "istarx.cc";
+    private static final int TIMEOUT = 3000;
+    private UiDevice mDevice;
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() {
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+    }
+
+    @Test
+    public void loginTest() {
+        mDevice.findObject(By.res(PKG + ":id/email")).setText(EMAIL);
+        mDevice.findObject(By.res(PKG + ":id/password")).setText(PASSWORD);
+        mDevice.findObject(By.res(PKG + ":id/login")).clickAndWait(Until.newWindow(), TIMEOUT);
+
+        String expectedString = String.format("Email is: %s\nPassword is: %s", EMAIL, PASSWORD);
+        String actualString = mDevice.findObject(By.res(PKG + ":id/intent_content")).getText();
+
+        assertEquals("Intent message error...", expectedString, actualString);
     }
 }
