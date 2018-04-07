df296df9958f03c16af66615e2dde5f1c067cf63
cherlas <vm@li.cm>
2018-04-07 21:02:54 +0800

    add login activity

--- app/src/main/java/cc/istarx/uiautomator/LoginActivity.java
+++ app/src/main/java/cc/istarx/uiautomator/LoginActivity.java
@@ -4,18 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-class LoginActivity extends AppCompatActivity{
+public class LoginActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_login);
 
         Intent intent = getIntent();
         String email = intent.getStringExtra("email");
         String password = intent.getStringExtra("password");
 
-
+        TextView textView = findViewById(R.id.intent_content);
+        textView.setText(String.format("Email is: %s\nPassword is: %s", email, password));
     }
 }
